f8d8e4cc6db7c267e8277d06cb6f6c68cba24f61
Amirtha <amirtha4501@gmail.com>
2021-03-07 13:02:53 +0530

    Changed DB Credentials to connection string

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,12 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: dbConfig.type,
-    host: process.env.DB_HOST || dbConfig.host,
-    port: process.env.DB_PORT || dbConfig.port,
-    username: process.env.DB_USERNAME || dbConfig.username,
-    password: process.env.DB_PASSWORD || dbConfig.password,
-    database: process.env.DB_NAME || dbConfig.database,
+    url: process.env.DATABASE_URL || dbConfig.DATABASE_URL,
+//     host: process.env.DB_HOST || dbConfig.host,
+//     port: process.env.DB_PORT || dbConfig.port,
+//     username: process.env.DB_USERNAME || dbConfig.username,
+//     password: process.env.DB_PASSWORD || dbConfig.password,
+//     database: process.env.DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true || dbConfig.synchronize
 };
